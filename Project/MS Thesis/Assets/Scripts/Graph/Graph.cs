ea2e6ce4cc3d835e307b3f429a4f229aff4fbfb7
darrare <ryandarras@gmail.com>
2019-01-07 16:44:09 -0700

    Added abstract graph class so I can easily hold different graph types in the Unity side of things

---
+++ Project/MS Thesis/Assets/Scripts/Graph/Graph.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Graph
+{
+    public abstract class GraphClass
+    {
+        /// <summary>
+        /// Checks the graph to see if it is satisfied
+        /// </summary>
+        public abstract bool CheckSatisfiability();
+
+        /// <summary>
+        /// Destroy the entirety of the graph. All objects included
+        /// </summary>
+        public abstract void Deconstruct();
+    }
+}
--- Project/MS Thesis/Assets/Scripts/MonoBehaviour/UIControllerScript.cs
+++ Project/MS Thesis/Assets/Scripts/MonoBehaviour/UIControllerScript.cs
@@ -13,21 +13,24 @@ public class UIControllerScript : MonoBehaviour
     [SerializeField]
     InputField unitSpherePercentDensity;
 
+    GraphClass graph;
+
     public void Lattice2DButtonClicked()
     {
-        new LatticeGraph2D<ColoredNode>(100, 100);
+        if (graph != null)
+            graph.Deconstruct();
+        graph = new LatticeGraph2D<ColoredNode>(100, 100);
     }
 
     public void UnitSphereGraphButtonClicked()
     {
-        try
-        {
-            new UnitSphereGraph<ColoredNode>(int.Parse(unitSphereNumNodes.text), float.Parse(unitSpherePercentDensity.text) / 100);
-        }
-        catch (Exception e)
-        {
-            throw new NotImplementedException("TODO: give a warning message somewhere on the interface that they entered invalid input.");
-        }
-        
+        if (graph != null)
+            graph.Deconstruct();
+        graph = new UnitSphereGraph<ColoredNode>(int.Parse(unitSphereNumNodes.text), float.Parse(unitSpherePercentDensity.text) / 100);       
+    }
+
+    public void CheckSatisfiability()
+    {
+        bool somethingReturned = graph.CheckSatisfiability();
     }
 }
