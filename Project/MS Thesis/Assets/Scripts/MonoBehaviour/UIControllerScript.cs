b55177c01bb3b50b4c44574d40b9644c4176039d
darrare <ryandarras@gmail.com>
2019-01-07 15:24:56 -0700

    Updated scene with new option to create unit sphere graph

--- Project/MS Thesis/Assets/Scripts/MonoBehaviour/UIControllerScript.cs
+++ Project/MS Thesis/Assets/Scripts/MonoBehaviour/UIControllerScript.cs
@@ -2,11 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Assets.Scripts.Graph;
+using UnityEngine.UI;
+using System;
 
 public class UIControllerScript : MonoBehaviour
 {
+    [SerializeField]
+    InputField unitSphereNumNodes;
+
+    [SerializeField]
+    InputField unitSpherePercentDensity;
+
     public void Lattice2DButtonClicked()
     {
         new LatticeGraph2D<ColoredNode>(100, 100);
+    }
+
+    public void UnitSphereGraphButtonClicked()
+    {
+        try
+        {
+            new UnitSphereGraph<ColoredNode>(int.Parse(unitSphereNumNodes.text), float.Parse(unitSpherePercentDensity.text));
+        }
+        catch (Exception e)
+        {
+            throw new NotImplementedException("TODO: give a warning message somewhere on the interface that they entered invalid input.");
+        }
+        
     }
 }
